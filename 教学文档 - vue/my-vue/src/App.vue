60022509b3c57ad81dee5e5a4fb0ac92df94dd6b
bingfengjw <498616031@qq.com>
2017-10-23 18:27:13 +0800

    es6

--- 教学文档 - vue/my-vue/src/App.vue
+++ 教学文档 - vue/my-vue/src/App.vue
@@ -9,30 +9,30 @@
 </template>
 
 <script>
-  import child from './components/Child.vue';
-  export default {
-    name: 'app',
-    data(){
-      return {
-        parentMsg:"这是使用动态绑定父组件传入子组件的值",
-        num:0
-      }
-    },
-    methods:{
-      showMsgFromChild:function (data) {
-        this.num = data;
-      }
-    },
-    components:{
-      child
+import child from "./components/Child.vue";
+export default {
+  name: "app",
+  data() {
+    return {
+      parentMsg: "这是使用动态绑定父组件传入子组件的值",
+      num: 0
+    };
+  },
+  methods: {
+    showMsgFromChild: function(data) {
+      this.num = data;
     }
+  },
+  components: {
+    child
   }
+};
 </script>
 
 <style scoped>
-  .father {
-    border:3px solid skyblue;
-    padding: 5px;
-    margin-bottom: 3px;
-  }
+.father {
+  border: 3px solid skyblue;
+  padding: 5px;
+  margin-bottom: 3px;
+}
 </style>
